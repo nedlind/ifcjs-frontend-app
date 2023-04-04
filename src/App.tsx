5a804972632690a8f01e113bc780c4a22d9d022c
Niklas Edlind <niklas.edlind@gmail.com>
2023-04-04 08:28:08 +0200

    routing & firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BuildingViewer } from './components/building-viewer';
+import { LoginForm } from './components/login-form';
+import { MapViewer } from './components/map-viewer';
 
 function App() {
-  return <h1>Hello World!</h1>
+  return <Router>
+    <Routes>
+      <Route path="/building" element={<BuildingViewer/>}/>
+      <Route path="/map" element={<MapViewer/>}/>
+      <Route path="/login" element={<LoginForm/>}/>
+      <Route path="/" element={<LoginForm/>}/>
+    </Routes>
+  </Router>
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { initializeApp } from "firebase/app";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDoBOHBMEE7DniZdki8Al0JBFilzwazgcA",
+  authDomain: "ifcjs-frondend-app.firebaseapp.com",
+  projectId: "ifcjs-frondend-app",
+  storageBucket: "ifcjs-frondend-app.appspot.com",
+  messagingSenderId: "374953526896",
+  appId: "1:374953526896:web:575ee3a522979d6a1356b5"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
 root.render(
   <React.StrictMode>
     <App />
